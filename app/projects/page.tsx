563c010a53c9ad954125943f2969d28f0e5aef8f
Iryna Howarth <irynahowarth@gmail.com>
2024-12-12 13:27:43 +0000

    [chore]: add basic routes for Projects and Tasks. add basic navigation to Layout

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+      <div className=" bg-gray-50">
+        <h1 className="text-4xl font-bold text-gray-600">
+        Projects Management Page
+        </h1>
+      </div>
+    );
+  }
---
+++ app/tasks/page.tsx
@@ -0,0 +1,9 @@
+export default function Tasks() {
+    return (
+      <div className=" bg-gray-50">
+        <h1 className="text-4xl font-bold text-gray-600">
+        Task Management Page
+        </h1>
+      </div>
+    );
+  }
